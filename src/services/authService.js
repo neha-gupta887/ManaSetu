5bdfc727ff685eb1fb6ead9eb425a36ccbf78c99
neha-gupta887 <nehaguptaj65@gmail.com>
2026-07-31 17:44:32 +0530

    Refactor Firebase auth service for user profile support

--- src/services/authService.js
+++ src/services/authService.js
@@ -10,18 +10,28 @@ import { auth } from "./firebase";
 
 const googleProvider = new GoogleAuthProvider();
 
-export const signup = (email, password) => {
-  return createUserWithEmailAndPassword(auth, email, password);
+// Create New Account
+export const signup = async (email, password) => {
+  const userCredential = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+
+  return userCredential;
 };
 
+// Email & Password Login
 export const login = (email, password) => {
   return signInWithEmailAndPassword(auth, email, password);
 };
 
+// Google Login
 export const googleLogin = () => {
   return signInWithPopup(auth, googleProvider);
 };
 
+// Logout
 export const logout = () => {
   return signOut(auth);
 };
