5b3b47c90b0e0cd589e60b9ddc8ba7c78c4f8489
neha-gupta887 <nehaguptaj65@gmail.com>
2026-08-05 00:54:07 +0530

    refactor(notifications): pass mark as read handler to NotificationList

--- src/components/notifications/NotificationList.jsx
+++ src/components/notifications/NotificationList.jsx
@@ -1,16 +1,18 @@
 import NotificationCard from "./NotificationCard";
 
-function NotificationList({ notifications }) {
+function NotificationList({
+  notifications,
+  onMarkAsRead,
+}) {
   return (
     <div className="mt-6 space-y-4">
-
       {notifications.map((notification) => (
         <NotificationCard
           key={notification.id}
           notification={notification}
+          onMarkAsRead={onMarkAsRead}
         />
       ))}
-
     </div>
   );
 }
